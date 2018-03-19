c9c4932e80259c578131a0fdb832ad046767fbd5
ceciliamalmrud <35834805+ceciliamalmrud@users.noreply.github.com>
2018-03-19 14:20:07 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,19 +1,19 @@
-docker stop pgserver || true && docker rm pgserver || true
-docker stop goserver || true && docker rm goserver || true
+docker stop postgres1 || true && docker rm postgres1 || true
+docker stop goapp1 || true && docker rm goapp1 || true
 
 docker build -t go GoApplication
 
 docker build -t pg PostgreSQL
 
-docker run --name pgserver -it -d pg
+docker run --name postgres1 -it -d pg
 
 sleep 5
 
-docker run --name goserver --link pgserver:db go go test
+docker run --name goapp1 --link postgres1:db go go test
 
 sleep 5
 
-docker stop pgserver
-docker stop goserver
-docker rm pgserver
-docker rm goserver
+docker stop postgres1
+docker stop goapp1
+docker rm postgres1
+docker rm goapp1
